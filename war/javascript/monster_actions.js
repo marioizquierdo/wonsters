2710f57ff8619080326ab2088dd0d131abef8bd1
marioizquierdo <tothemario@gmail.com>
2010-09-06 20:36:15 +0200

    refs #47. Añadido JavaScript básico para ir actualizando el targetValue de las tareas al pulsar en el botón. Pequeño cambio al CSS también.

--- war/javascript/monster_actions.js
+++ war/javascript/monster_actions.js
@@ -14,7 +14,7 @@ $(function(){
 	
 	// Turnos libres que hay actualmente en la pantalla (este es dinamico)
 	var freeTurnsValue = function() {
-	    return parseInt($('span.free_turns_value').text()) || 0;
+	    return parseInt($('#monster_tasks span.free_turns_value').text()) || 0;
 	};
 	
 	//Turnos libres del mounstro en total. (Este es estatico y no cambia)
@@ -32,7 +32,7 @@ $(function(){
 	var addMaxTurns= function (target_input){
 	    var turns_to_use = 0;
 	    //sumamos todos los inputs menos el que nos pasan
-	    $('input.turns_input').each(function() {
+	    $('#monster_tasks input.turns_input').each(function() {
 	        var turns_input = $(this)
 	        if (turns_input.attr('id') != target_input.attr('id')){
 	            turns_to_use += parseInt(turns_input.val()) || 0;
@@ -52,66 +52,91 @@ $(function(){
 	
 	
 	// Function para actualizar los turnos libres en funcion de lo que hay en los inputs. 
-	//El target_input es el input que se ha cambiado.
+	// El target_input es el input que se ha cambiado.
 	var refreshFreeTurns = function(target_input) {
+		// Calcular turnos usados
 	    var turns_to_use = 0;
-	    $('input.turns_input').each(function() {
+	    $('#monster_tasks input.turns_input').each(function() {
 	        var turns_input = $(this);
 	        turns_input.val(validarInput(turns_input.val()));
 	        turns_to_use += parseInt(turns_input.val()) || 0;
 	    });
 	    
+	    // Calcular turnos libres
 	    var turns_avaliable = monster_free_turns - turns_to_use;
-	    if (turns_avaliable<0 && (target_input != undefined)){
+	    if ((turns_avaliable < 0) && (target_input != undefined)) {
 	        addMaxTurns(target_input);
 	        turns_avaliable = 0;
 	    }
 	    
-	    if (turns_avaliable>0) $('span.free_turns_value').css('color','black');
-	    else $('span.free_turns_value').css('color','red').effect('pulsate',[],200);
+	    // Poner turnos libres en 'span.free_turns_value'
+		$('#monster_tasks span.free_turns_value').text(turns_avaliable);
+	    if (turns_avaliable > 0) {
+	    	$('#monster_tasks span.free_turns_value').css('color', 'black');
+	    } else {
+	    	$('#monster_tasks span.free_turns_value').css('color', 'red').effect('pulsate', [], 200);
+	    }
 	    
-	   $('span.free_turns_value').text(turns_avaliable);
-	   return false;
+	    // Cambiar el targetValue correspondiente a la tarea del input.
+	    if(target_input) refreshTargetValue(target_input);
+	    
+	    return false;
 	};
-	 
-    var addCuantityInput = function(target_input,cantidad) {
-    	cantidad = parseInt(cantidad) || 0;
+	
+	// Refresca el valor del targetValue correspondiente a la tarea del input.
+	var refreshTargetValue = function(target_input) {
+		var action = target_input.parents('.monsterActionSuggestion');
+		var action_attr = function(attr_name) {
+			return parseInt(action.find('.suggested_action_extra_data .MASuggestion_' + attr_name).text()) || 0;
+		}
+		
+	    var targetValue = action_attr('targetValue'); // Valor del targetVaue original (sin modificar)
+	    var targetValueIncreasePerTurn = action_attr('targetValueIncreasePerTurn'); // Incremento por cada turno gastado
+	    var turnsToUse = parseInt(target_input.val()) || 0;
+	    
+	    var newTargetValue = targetValue + turnsToUse * targetValueIncreasePerTurn;
+	    action.find('.targetValueInfo span.targetValue').text(newTargetValue);
+	}
+	
+	// Añadir cierta cantidad al input de la acción
+    var addCuantityInput = function(target_input, cuantity) {
+    	cuantity = parseInt(cuantity) || 0;
     	var target_input_value = validarInput(target_input.val());
-    	target_input.val(target_input_value + cantidad);
+    	target_input.val(target_input_value + cuantity);
     	refreshFreeTurns(target_input);
     	return false;
     };
 	                
 	// Comportamiento del boton de anhadir un turno mas
 	$('form[name=monsterActionsToDoForm] button.add_turn_button').click(function(){
-	    var target_input = '#' + $(this).attr('id').replace('_add_turn_button','_turns_input');
-	    addCuantityInput($(target_input),1);
+	    var target_input = '#' + $(this).attr('id').replace('_add_turn_button', '_turns_input');
+	    addCuantityInput($(target_input), 1);
 	    return false;
 	});
 	
 	// Comportamiento del boton de quitar un turno
 	$('form[name=monsterActionsToDoForm] button.remove_turn_button').click(function(){
-	   var target_input = '#' + $(this).attr('id').replace('_remove_turn_button','_turns_input');
-	    addCuantityInput($(target_input),-1);
+	   var target_input = '#' + $(this).attr('id').replace('_remove_turn_button', '_turns_input');
+	    addCuantityInput($(target_input), -1);
 	    return false;
 	});
 	
 	// Comportamiento del boton de anhadir todos los turnos
 	$('form[name=monsterActionsToDoForm] button.add_all_button').click(function(){
-	   var target_input = '#' + $(this).attr('id').replace('_add_all_button','_turns_input');
+	   var target_input = '#' + $(this).attr('id').replace('_add_all_button', '_turns_input');
 	   addMaxTurns($(target_input));
 	   return false;
 	});
 	
 	//Comportamiento del boton de quitar todos los turnos
 	$('form[name=monsterActionsToDoForm] button.remove_all_button').click(function(){
-	    var target_input = '#' + $(this).attr('id').replace('_remove_all_button','_turns_input');
+	    var target_input = '#' + $(this).attr('id').replace('_remove_all_button', '_turns_input');
 	    removeAllTurns($(target_input));
 	    return false;
 	});
 	                
 	// Comportamiento cuando se cambia el valor de un input a mano     
-	$('input.turns_input').bind('change keyup click paste', function(e){
+	$('form[name=monsterActionsToDoForm] input.turns_input').bind('change keyup click paste', function(e){
 	    refreshFreeTurns($(this));
 	    return false;
 	});
