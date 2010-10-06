550f50a248c8f43a7c8d00af47944ebd0b50c859
marioizquierdo <tothemario@gmail.com>
2010-10-06 23:09:00 +0200

    refs t07.1. Retocadas las ayudas de los atributos del monstruo. Ahora ya se pueden leer bien.

--- war/javascript/fix_attrs_descriptions.js
+++ war/javascript/fix_attrs_descriptions.js
@@ -1,21 +1,9 @@
 $( function() {
-	var BRIEF_LENGTH = 55;
-	var HIDDEN_DESC_CLASS = "hidden_attribute_description";
-		
-	$('.attribute_row .attribute_description').each(function() {
-		var e = $(this);
-		var description = e.text();
-		var bief = description.substring(0, BRIEF_LENGTH);
-		e.text(bief);
-		if(bief.length == BRIEF_LENGTH) {
-			e.append('...');
-			e.append('<div class="'+HIDDEN_DESC_CLASS+' attribute_details_content"'+
-			' style="display:none">'+description+'</div>')
-		} else {
-			e.append('<div class="'+HIDDEN_DESC_CLASS+'"/>')
-		}
-	});
 	
+	/**
+	 * Poner en el texto del tooltip como HTML con icono
+	 */
+	var HIDDEN_DESC_CLASS = "hidden_attribute_description";
 	$('.attribute_row').each(function() {
 		var e = $(this);
 		e.find('.attribute_level, .attribute_name, .attribute_description')
@@ -24,12 +12,9 @@ $( function() {
 			    delay: 100,
 			    fadeIn: 100,
 			    fadeOut: 400,
+			    maxWidth: '300px',
 			    defaultPosition: 'left',
-			    content: '<h3>'+ e.find('.hidden_m_icon').html() +'&nbsp;'+
-		        	e.find('.attribute_name').text() +'</h3>'+ 
-		        	'<div>'+ e.find('.'+HIDDEN_DESC_CLASS).text() +'</div>'+
-		        	e.find('.hidden_attribute_details').html()
-	    	})
-	    	;
+			    content: e.find('.hidden_attribute_details').html()
+	    	});
 	});
 });
