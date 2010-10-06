dafd666da249c7d3baf5b4d74abbf705b43d2534
diegodl <diegodl84@gmail.com>
2010-10-06 15:33:05 +0200

    #55 Actualizado MonsterName. Genera hasta 350 nombres diferentes

--- src/main/java/es/engade/thearsmonsters/model/util/MonsterName.java
+++ src/main/java/es/engade/thearsmonsters/model/util/MonsterName.java
@@ -2,8 +2,31 @@ package es.engade.thearsmonsters.model.util;
 
 public class MonsterName {
 	
+	private static final int COMPOSE_PROBABILITY = 80;
+	
+	public static final String[] PREFIX_NAMES = {
+		"Light", "Heavy", "Fast", "Slow", "Spinning",
+		"Fuzzy", "Hard", "Coarse", "Brute",
+		"Frozen", "Liquid", "Solid", "Plasma", "Plush", 
+		"Golden", "Silver", "Bronze", "Steel",
+		"Iron", "Wooden", "Copper", "Metal", "Plastic",
+		"Hidden", "Stealth",
+		"Sad", "Cry", "Lazy", "Happy"
+	};
+	
+	public static final String[] SUFFIX_LIGHT_NAMES = {
+		"Cherry", "Jenny", "Mickey", "Bear",
+		"Viking", "Toy", "Lizard", "Moose",
+		"Cat", "Cray", "Jelly"
+	};
+	
+	public static final String[] SUFFIX_HARD_NAMES = {
+		"Warrior", "Lord", "Ogre", "Orc", "Berserker",
+		"Hunter", "Blade"
+	};
+	
 	public static final String[] NAMES_DATABASE = {
-		"Alfonsito",
+		"Alfonsito", 
 		"Ángel",
 		"Bumble",
 		"Castro",
@@ -39,7 +62,14 @@ public class MonsterName {
 	 * @return a random Monster Name picked up from the NAMES_DATABASE constant list.
 	 */
 	public static final String random() {
-		int randomIndex = (int) (Math.random() * NAMES_DATABASE.length);
-		return NAMES_DATABASE[randomIndex];
+		int randomType = (int) (Math.random() * 100);
+		if (randomType < COMPOSE_PROBABILITY) {
+			int randomPrefix = (int) (Math.random() * PREFIX_NAMES.length);
+			int randomSuffix = (int) (Math.random() * SUFFIX_LIGHT_NAMES.length);
+			return PREFIX_NAMES[randomPrefix] + SUFFIX_LIGHT_NAMES[randomSuffix];
+		} else {
+			int randomIndex = (int) (Math.random() * NAMES_DATABASE.length);
+			return NAMES_DATABASE[randomIndex];
+		}
 	}
 }
